040b4a3e33179e9e57b3f33d976ce4593c8df184
Nagata-Ryo <59526430+Nagata-Ryo@users.noreply.github.com>
2020-01-31 14:46:51 +0900

    Add files via upload

---
+++ rensohairetsuhw.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>連想配列hw</title>
+</head>
+<body>
+<?php
+
+$whisky_price = array(
+  '響21年'=>'25,000',
+  'マッカラン12年'=>'7,000',
+  'シーバスリーガル12年'=>'2,500',
+  'タリスカー10年'=>'4,000',
+  'ベル'=>'1,200',);
+foreach ($whisky_price as $name => $price) {
+  echo $name."は".$price."円です";// code...
+}
+
+ ?>
+</body>
+</html>

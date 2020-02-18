7bb13ed749032fc9158ed9c9f8d57246ef62adb2
Nagata-Ryo <59526430+Nagata-Ryo@users.noreply.github.com>
2020-02-18 16:26:19 +0900

    jannken.phpを追加します

--- jannken.php
+++ jannken.php
@@ -1,5 +1,7 @@
 <?php
+
 $result = false;
+
 
 if (isset($_POST['choice'])) {
     $janken = array(
@@ -8,9 +10,11 @@ if (isset($_POST['choice'])) {
         'パー'
     );
 
+
     $player = htmlspecialchars($_POST['choice'], ENT_QUOTES, 'UTF-8');
 
     $com = $janken[array_rand($janken)];
+
 
     if ($player === 'グー' && $com === 'チョキ') {
         $result = '勝ち';
@@ -18,50 +22,47 @@ if (isset($_POST['choice'])) {
         $result = 'あいこ';
     } elseif ($player === 'グー' && $com === 'パー') {
         $result = '負け';
-
-    }elseif ($player === 'チョキ' && $com === 'チョキ') {
-        $result = 'あいこ';
     } elseif ($player === 'チョキ' && $com === 'グー') {
         $result = '負け';
+    } elseif ($player === 'チョキ' && $com === 'チョキ') {
+        $result = 'あいこ';
     } elseif ($player === 'チョキ' && $com === 'パー') {
         $result = '勝ち';
-
-    }elseif ($player === 'パー' && $com === 'チョキ') {
-        $result = '負け';
     } elseif ($player === 'パー' && $com === 'グー') {
         $result = '勝ち';
+    } elseif ($player === 'パー' && $com === 'チョキ') {
+        $result = '負け';
     } elseif ($player === 'パー' && $com === 'パー') {
         $result = 'あいこ';
+    }
+
 }
 ?>
-
-
 <!DOCTYPE html>
-<html>
+<html lang="ja">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>じゃんけん</title>
+<meta charset="UTF-8">
+<title>PHPでじゃんけんゲーム</title>
 </head>
 <body>
-<?php
-  echo "選んでください";
-?>
- <form action="" method="post">
-      <button type="submit" name="choice" value="グー">グー</button>
-      <button type="submit" name="choice" value="チョキ">チョキ</button>
-      <button type="submit" name="choice" value="パー">パー</button>
-  </form>
+<p>選んでください。</p>
 
+<form action="" method="post">
+    <button type="submit" name="choice" value="グー">グー</button>
+    <button type="submit" name="choice" value="チョキ">チョキ</button>
+    <button type="submit" name="choice" value="パー">パー</button>
+</form>
 
-<?php   if ($result) :
-      echo "結果";
-?>
+<?php if ($result) : ?>
+    <p>結果</p>
 
-  <?php      echo "あなた：".$player;
-             echo "あいて：".$com;
+    <p>
+    あなた：<?php echo $player; ?><br>
+    あいて：<?php echo $com; ?>
+    </p>
 
+    <p><?php echo $result; ?>です。</p>
+<?php endif; ?>
 
-     echo $result; です。
-    ?>
 </body>
 </html>
